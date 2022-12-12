f36f4cf7e0427afec3f2dc554dac2331ed71dc3c
Saffanah Alkhathlan <116809082+Saffanahk@users.noreply.github.com>
2022-12-12 16:38:54 +0300

    Initial Commit

---
+++ SaffanahGitub/SaffanahGitubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaffanahGitubApp.swift
+//  SaffanahGitub
+//
+//  Created by Saffanah Alkhathlan on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaffanahGitubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
